38c7b5841491cab2cc2c506efa2e0e6ae67baac2
ArunLohani <arun.lohani@argusoft.in>
2025-09-20 10:21:07 +0530

    Add Cloud Server and Client

---
+++ CloudConfigClientOne/src/test/java/com/springCloudConfigClientOne/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springCloudConfigClientOne.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
